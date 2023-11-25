3764dacca4231a4d52dadef8c4cb774404982e5d
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-25 08:42:13 +0000

    Add files via upload

---
+++ oct_hex_bin.py
@@ -0,0 +1,18 @@
+print(hex(16))
+#will print the number in hex
+print(oct(54))
+#will print the number on oct
+print(bin(65))
+#will print the number on bin
+print(format(16,"x"))
+#\this will print number on hex
+print(format(54,"o"))
+#display the number on octale
+print(format(65,"b"))
+#display the number on binary
+print(int("ff",16))
+#change number from hex to dec
+print(int("337",8))
+#change number from oct to dec
+print(int("010100011100111010110",2))
+#change number from oct to dec
